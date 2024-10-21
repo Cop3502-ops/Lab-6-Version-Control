96dfe67ff36187f120687a96d83ebd0efe4f9e15
COP3502-ops <duddelaaatish@gmail.com>
2024-10-21 15:56:09 -0400

    Stored the encoder method in a variable

--- lab6_coders.py
+++ lab6_coders.py
@@ -3,9 +3,6 @@ def encode(password):
     encodePassword = ""
     for digit in password:
         newDigit = (int(digit) + 3) % 10
-        #The password is encoded by adding each individual digit by 3.
-        #For two-digit numbers, the number is encoded using modulus 10 to get the last digit
-        #Type-casting is needed because strings and integers alone do not concatenate with each other
         encodePassword += str(newDigit)
 
     return encodePassword
@@ -13,20 +10,18 @@ def encode(password):
 def main():
     continueCode = True
     while continueCode:
-        print("Menu") #Prints the menu options
+        print("Menu")
         print("-------------")
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
         print("")
-        menuChoice = input("Please enter an option:") #Asks user for an option
+        menuChoice = input("Please enter an option:")
         if menuChoice == "1":
             passEncoder = input("Please enter your password to encode:")
-            encode(passEncoder)
-            #Removed the print statement which displays the encoded password
-            #print(encode(passEncoder))
+            encodeVar = encode(passEncoder)
             print("Your password has been encoded and stored!")
-            print("") #Added a new line when reprinting the menu
+            print("")
         elif menuChoice == "2":
             pass
 
